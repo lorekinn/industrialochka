194dd53b36c9e95e740ea106432e4cdb4798c5c4
lorekinn <125356263+lorekinn@users.noreply.github.com>
2023-02-20 22:11:59 +0300

    Create triangle.h

---
+++ 2 practice/Triangle/triangle.h
@@ -0,0 +1,13 @@
+class Triangle {
+private:
+	double a, b, c;
+
+public:
+	Triangle(double a1, double b1, double c1);
+	Triangle();
+	bool exst_tr();
+	void set(double a1, double b1, double c1);
+	void show();
+	double perimeter();
+	double square();
+};
