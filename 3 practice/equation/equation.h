6c1e65cfdf745665610aa0c98b538744ae6fef79
lorekinn <125356263+lorekinn@users.noreply.github.com>
2023-02-27 23:06:40 +0300

    Create equation.h

---
+++ 3 practice/equation/equation.h
@@ -0,0 +1,13 @@
+class equation {
+	double a;
+	double b;
+	double c;
+	double D;
+public:
+	equation();
+	equation(double _a, double _b, double _c);
+	void set(double _a, double _b, double _c);
+	void show();
+	double find_X();
+	double find_Y(double a1);
+};
