c8a7a6af5a80f1d156e9485720dbd90fb2d142a8
lorekinn <125356263+lorekinn@users.noreply.github.com>
2023-03-26 22:29:51 +0300

    Create ocean.h

---
+++ 6 practice/ocean/ocean.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class ocean {
+protected:
+	string name, location;
+	int depth, square;
+public:
+	ocean();
+	ocean(string oc);
+	ocean(string n, string l, int d, int s);
+	friend ostream& operator<< (ostream& stream, ocean o);
+	friend istream& operator>> (istream& stream, ocean& o);
+}
