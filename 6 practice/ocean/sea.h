209b5acd485c6e1fec1d0cf4353aaf22fdbcaa13
lorekinn <125356263+lorekinn@users.noreply.github.com>
2023-03-26 22:30:30 +0300

    Create sea.h

---
+++ 6 practice/ocean/sea.h
@@ -0,0 +1,13 @@
+#include "ocean.h"
+
+class sea : public ocean {
+protected:
+	string sname;
+	int sdepth, ssquare, cline;
+public:
+	sea();
+	sea(string n, string sn);
+	sea(string oc, string n, int d, int s, int l);
+	friend ostream& operator<< (ostream& stream, sea o);
+	friend istream& operator>> (istream& stream, sea& o);
+}
