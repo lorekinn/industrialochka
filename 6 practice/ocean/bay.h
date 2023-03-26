0ec9bc26d85790695c0723913dcd3791df3415e6
lorekinn <125356263+lorekinn@users.noreply.github.com>
2023-03-26 22:30:50 +0300

    Create bay.h

---
+++ 6 practice/ocean/bay.h
@@ -0,0 +1,11 @@
+#include "sea.h"
+
+class bay : public sea {
+	string bname;
+	int length, height;
+public:
+	bay();
+	bay(string n, string sn, string bn, int l, int h);
+	friend ostream& operator<< (ostream& stream, bay s);
+	friend istream& operator>> (istream& stream, bay& s);
+}
