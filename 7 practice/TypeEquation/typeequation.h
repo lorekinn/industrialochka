a2b616d144af4e5ec10fc267d5b9bfaafa9756cc
lorekinn <125356263+lorekinn@users.noreply.github.com>
2023-04-02 22:57:43 +0300

    Create typeequation.h

---
+++ 7 practice/TypeEquation/typeequation.h
@@ -0,0 +1,14 @@
+class Type1Equation {
+public:
+    void Get_answer();
+    void show();
+};
+
+class Type2Equation {
+private:
+    double A;
+public:
+    Type2Equation(double a1);
+    void Get_answer();
+    void show();
+};
