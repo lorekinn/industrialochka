82d278320eadf35b327b7e8bc886a03853fb5f37
lorekinn <125356263+lorekinn@users.noreply.github.com>
2023-04-30 14:38:26 +0300

    Create type.h

---
+++ 10 practice/type/type.h
@@ -0,0 +1,20 @@
+
+#include <cmath>
+
+class Type5 {
+    double B, C;
+
+public:
+    Type5(double b1, double c1);
+    void Get_answer();
+    void show();
+};
+
+class Type6 {
+    double A, B, C;
+
+public:
+    Type6(double a1, double b1, double c1);
+    void Get_answer();
+    void show();
+};
