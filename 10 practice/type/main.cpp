41d5e7d250b45789a630b8c45eeeed9d43f33dff
lorekinn <125356263+lorekinn@users.noreply.github.com>
2023-04-30 14:38:48 +0300

    Create main.cpp

---
+++ 10 practice/type/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "type.h"
+using namespace std;
+
+int main() {
+    Type5 eq1(2, -8);
+    eq1.show();
+    eq1.Get_answer(); 
+
+    Type6 eq2(1, -5, 6);
+    eq2.show(); 
+    eq2.Get_answer();
+
+}
