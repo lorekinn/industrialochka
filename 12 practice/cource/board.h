4175d5e48029ce14d39ffc8e8c75c1eb763b18bc
lorekinn <125356263+lorekinn@users.noreply.github.com>
2023-05-14 11:53:38 +0300

    Create board.h

---
+++ 12 practice/cource/board.h
@@ -0,0 +1,21 @@
+#pragma once
+#define MAXSIDE 25
+
+class Board {
+private:
+	int SIDE, MINES;
+public:
+	int countMines(int row, int col, int mines[][2], char realBoard[][MAXSIDE]);
+	bool checkBoard(char GameBoard[][MAXSIDE], char realBoard[][MAXSIDE], int mines[][2], int row, int col, int* movesLeft);
+	bool isValid(int row, int col);
+	bool isMine(int row, int col, char board[][MAXSIDE]);
+	void Move(int* x, int* y);
+	void RandomMove(int* x, int* y, int moves[][2], int moveIndex);
+	void GenerateMove(int moves[][2], int movesLeft);
+	void printBoard(char GameBoard[][MAXSIDE]);
+	void endBoard(char GameBoard[][MAXSIDE]);
+	void place(int mines[][2], char realBoard[][MAXSIDE]);
+	void initialise(char realBoard[][MAXSIDE], char GameBoard[][MAXSIDE]);
+	void replace(int row, int col, char board[][MAXSIDE]);
+  void play();
+}
